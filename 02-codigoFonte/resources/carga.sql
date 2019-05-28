8d9df985f705493e188bf448a9629a6a18702b87
Taciano Amorim <tacianolamorim@gmail.com>
2019-05-28 09:37:51 -0300

    Montagem da estrutura

--- 02-codigoFonte/resources/carga.sql
+++ 02-codigoFonte/resources/carga.sql
@@ -1,3 +1,4 @@
 USE DBSPJC;
 
-INSERT INTO juizado (codigo_curso, nome) VALUES (1,'Engeharia da Computacao');
+INSERT INTO juizado (codigo_curso, nome) VALUES (1,'Juizados Civeis da Capital');
+INSERT INTO juizado (codigo_curso, nome) VALUES (2,'Juizados Civeis do Interior');
