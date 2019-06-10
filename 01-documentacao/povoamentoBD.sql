fb5a3d0bdd3dcacf0c0ddc1b6638abc954aa73b2
Taciano amorim <tacianolamorim@gmail.com>
2019-06-10 01:09:45 -0300

    ajustes no modelo de banco de dados

---
+++ 01-documentacao/povoamentoBD.sql
@@ -0,0 +1,12 @@
+USE DBSPJC;
+
+INSERT INTO TipoMovimentacao (id, nome) VALUES
+(1, 'Concluso para despacho'),
+(2, 'Concluso para decisão'),
+(3, 'Concluso para sentença'),
+(4, 'Finalizar PAJ'),
+(5, 'Inclusão em pauta'),
+(6, 'Petição'),
+(7, 'Mero expediente - Despacho'),
+(8, 'Julgamento'),
+(9, 'Distribuição');
