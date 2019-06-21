0f514304c5e12a8ba3518aa8d3da8d7d772ab843
Taciano Amorim <tacianolamorim@gmail.com>
2019-06-21 14:56:55 -0300

    Adicionadoos arquivo function de marcos

---
+++ 01-documentacao/marcos/function buscarPautaProxima.sql
@@ -0,0 +1,30 @@
+delimiter //
+DROP FUNCTION IF EXISTS DBSPJC.buscarPautaProxima //
+
+create function DBSPJC.buscarPautaProxima(dataRef VARCHAR(10)) 
+returns int
+deterministic
+begin
+	declare idPautaProxima int;
+    declare dataIgual date;
+    declare dataMaiorProxima date;
+    declare dataMenorProxima date;
+    
+	select dataAgendamento into dataIgual from Pauta where dataAgendamento = dataRef;
+	select MAX(dataAgendamento) into dataMenorProxima from Pauta where dataAgendamento < dataRef;
+	select MIN(dataAgendamento) into dataMaiorProxima from Pauta where dataAgendamento > dataRef;    
+    
+    if ( dataIgual = dataRef ) then
+		select id into idPautaProxima from Pauta where dataAgendamento = dataRef;
+        
+    elseif ( dataMenorProxima < dataRef ) then
+		select id into idPautaProxima from Pauta where dataAgendamento < dataRef;
+    else 
+		select id into idPautaProxima from Pauta where dataAgendamento > dataRef; 
+    end if;
+ 	   
+	return idPautaProxima;
+
+end; //
+
+# SELECT DBSPJC.buscarPautaProxima('2019-07-10');
