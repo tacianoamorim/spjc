8395e6dec56707b2a83df07d885e209247f8b78f
Taciano Amorim <tacianolamorim@gmail.com>
2019-06-30 08:26:54 -0300

    ajuste no relatorio

--- 01-documentacao/taciano/view juizadoPautaAudiencia.sql
+++ 01-documentacao/taciano/view juizadoPautaAudiencia.sql
@@ -4,10 +4,11 @@ delimiter //
 DROP VIEW IF EXISTS DBSPJC.juizadoPautaAudiencia; //
 
 CREATE VIEW DBSPJC.juizadoPautaAudiencia AS
-SELECT j.nome, pt.dataAgendamento, au.hora, au.processo 
+SELECT j.nome, pt.dataAgendamento, au.hora, au.processo, 
+	CONCAT( DATE_FORMAT(pt.dataAgendamento, '%d/%m/%Y'), ' ', 
+			DATE_FORMAT(au.hora, '%H:%i') ) AS dataHora
 FROM DBSPJC.Pauta pt
 	INNER JOIN DBSPJC.Audiencia au ON au.pauta= pt.id
     INNER JOIN DBSPJC.Processo prc ON prc.npu= au.processo
     INNER JOIN DBSPJC.Juizado j ON j.id = prc.juizado; //
-    
 #select * from DBSPJC.juizadoPautaAudiencia
--- 01-documentacao/taciano/view parteRepresentanteProcesso.sql
+++ 01-documentacao/taciano/view parteRepresentanteProcesso.sql
@@ -5,24 +5,44 @@ DROP VIEW IF EXISTS DBSPJC.parteRepresentanteProcesso; //
 
 CREATE VIEW DBSPJC.parteRepresentanteProcesso AS
 
-SELECT pp.processo, p.polo, 'P' AS 'Tipo', pf.nome AS 'nome' 
+SELECT pp.processo, 
+	(CASE p.polo
+		WHEN 'A' THEN 'Ativo'
+		ELSE 'Passivo'
+	END) AS 'polo',
+	'P' AS 'Tipo', CONCAT(pf.nome, ' (Parte)') AS 'nomePTR' 
 FROM DBSPJC.ProcessoParte pp
 	INNER JOIN DBSPJC.Parte p ON p.id= pp.parte
     INNER JOIN DBSPJC.ParteFisica pf ON pf.parte = p.id
 UNION
 
-SELECT ppj.processo, pj.polo, 'P' AS 'Tipo', pjr.razaoSocial AS 'nome'
+SELECT ppj.processo, 
+	(CASE pj.polo
+		WHEN 'A' THEN 'Ativo'
+		ELSE 'Passivo'
+	END) AS 'polo',
+	'P' AS 'Tipo', CONCAT(pjr.razaoSocial,' (Parte)') AS 'nomePTR' 
 FROM DBSPJC.ProcessoParte ppj
 	INNER JOIN DBSPJC.Parte pj ON pj.id= ppj.parte
     INNER JOIN DBSPJC.ParteJuridica pjr ON pjr.parte = pj.id
 UNION
 
-SELECT pr.processo, r.polo, 'R' AS 'Tipo', r.nome AS 'nome'
+SELECT pr.processo,	
+	(CASE r.polo
+		WHEN 'A' THEN 'Ativo'
+		ELSE 'Passivo'
+	END) AS 'polo',
+	'R' AS 'Tipo', CONCAT(r.nome, ' (Representante)') AS 'nomePTR' 
 FROM DBSPJC.ProcessoRepresentante pr
 	INNER JOIN DBSPJC.Representante r ON r.cpf= pr.representante
 UNION
 
-SELECT pt.processo, t.polo, 'T' AS 'Tipo', t.nome AS 'nome' 
+SELECT pt.processo, 
+	(CASE t.polo
+		WHEN 'A' THEN 'Ativo'
+		ELSE 'Passivo'
+	END) AS polo,
+	'T' AS 'Tipo', CONCAT(t.nome, ' (Testemunha)') AS 'nomePTR' 
 FROM DBSPJC.ProcessoTestemunha pt
 	INNER JOIN DBSPJC.Representante t ON t.cpf= pt.testemunha
 //
